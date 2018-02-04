2b81c5ed4ddf8ba07043e150ef12b1df3fac9825
Edwin <edwin_farley@brown.edu>
2018-02-03 19:03:05 -0500

    02/03/2018

---
+++ master.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  3 18:56:04 2018
+
+@author: Edwin
+"""
+
+import numpy as np
+import pandas as pd
+import pymc3 as pm
+import scipy as sp
+import math
+
+from perm_sample_norm import *
+from perm_sample_binom import *
+from perm_sample_poisson import *
+
+def glm_mcmc_inference(df, formula, family, I):
+    """
+    Calculates the Markov Chain Monte Carlo trace of
+    a Generalised Linear Model Bayesian linear regression
+    model on supplied data.
+
+    df: DataFrame containing the data
+    formula: Regressing equation in terms of columns of DataFrame df
+    family: Type of liner model. Takes a pymc object (pm.glm.families).
+    I: Number of iterations for MCMC
+
+    """
+    # Use PyMC3 to construct a model context
+    basic_model = pm.Model()
+    with basic_model:
+        # Create the glm using the Patsy model syntax
+        pm.glm.GLM.from_formula(str(formula), df, family=family)
+        start = pm.find_MAP()
+        step = pm.NUTS()
+
+        trace = pm.sample(I, step, start, progressbar=False)
+
+        return(trace)
+
+def build_permutation(p, arr):
+    new = [0 for i in range(len(arr))] 
+    l = len(p)
+    for i in range(l):
+            new[i] = arr[p[i]]
+    return(new)
+    
--- perm_sample_binom.py
+++ perm_sample_binom.py
@@ -16,14 +16,15 @@ Start over and repeat a set number of times.
 import numpy as np
 import pandas as pd
 import pymc3 as pm
+import scipy as sp
 
-from scipy.stats import norm, lognorm, poisson
+from scipy.stats import norm, lognorm, poisson, binom
 #from localsearch import *
 
 eps_sigma_sq = 1
 v=1
 
-def simulate_data_normal(N, B):
+def simulate_data_logistic(N, B):
     """
     Simulate a random dataset using a noisy
     linear process.
@@ -31,32 +32,29 @@ def simulate_data_normal(N, B):
     N: Number of data points to simulate
     B: Vector of regression parameters. Defines number of covariates. (Include B_0)
     """
-    # Create a pandas DataFrame with column 'x' containing
-    # N uniformly sampled values between 0.0 and 1.0
     seed = 7
 
     df = pd.DataFrame(
-        {str("x1"): np.random.RandomState(seed).normal(
-                0, v, N)})
+        {str("x1"): np.random.RandomState(seed).uniform(size = N)})
     for i in range(2, len(B)):
         print(i)
         df_i = pd.DataFrame(
-                {str("x" + str(i)): np.random.RandomState(seed+i).normal(
-                0, v, N)})
+                {str("x" + str(i)): np.random.RandomState(seed+i).uniform(size = N)})
         df = pd.concat([df, df_i], axis = 1)
 
 
     # Use a linear model (y ~ beta_0 + beta_1*x + epsilon) to
     # generate a column 'y' of responses based on 'x'
     #Betas are normally distributed with mean 0 and variance eps_sigma_sq
-    y = B[0] + np.matmul(pd.DataFrame.as_matrix(df), np.transpose(B[1:])) + np.random.RandomState(42).normal(0, eps_sigma_sq, N)
-    df["y"] =  y
+    p = np.exp(B[0] + np.matmul(pd.DataFrame.as_matrix(df), np.transpose(B[1:])))
+    p = p/(1+p)#+ np.random.RandomState(42).normal(0, eps_sigma_sq, N)
+    df["y"] = np.round(p)
 
     return df
 
 
-def normal_permute(df,formula, I, T):
-    family = 'Normal'
+def logistic_permute(df, formula, I, T):
+    family = 'Logistic'
     y1 = formula.split(' ~ ')[0]
     print(y1)
     
@@ -68,43 +66,29 @@ def normal_permute(df,formula, I, T):
     #P = eye(m)
     p = [i for i in range(m)]
     
-    if family == 'Normal':
-            with pm.Model() as P_model:
-                pm.glm.GLM.from_formula(formula, df, family = pm.glm.families.Normal())
-                trace = pm.sample(I, chains = 1, tune = 0, progress_bar = False)
-                print("Sampling done.")
-                
-            #trace = glm_mcmc_inference(df, formula, pm.glm.families.Normal(), I)
-            beta_names = ['Intercept']
-            beta_names.extend(formula.split(' ~ ')[1].split(' + '))
-            B = np.transpose([trace.get_values(s)[-1] for s in beta_names])
-            sd = trace.get_values('sd')[-1]
-    
-            x = np.matmul(A, B)
-    
-            likelihood = sum([np.log(l) for l in norm.pdf(x, y, sd)])
-            print(likelihood)
-    
-    elif family == 'Poisson':
-        trace = glm_mcmc_inference(df, formula, pm.glm.families.Poisson(), I)
+    if family == 'Logistic':
+        '''with pm.Model() as logistic_model:
+            pm.glm.GLM.from_formula('y ~ x1 + x2', df, family=pm.glm.families.Normal())
+            trace_logistic_model = pm.sample(I, chains=1, tune=0, progress_bar = False)'''
+        trace = glm_mcmc_inference(df, formula, pm.glm.families.Normal(), I)
         beta_names = ['Intercept']
         beta_names.extend(formula.split(' ~ ')[1].split(' + '))
         B = np.transpose([trace.get_values(s)[-1] for s in beta_names])
-        mu = trace.get_values('mu')[-1]
+        print(B)
         
         x = np.matmul(A, B)
-        
-        likelihood = np.sum([np.log(l) for l in poisson.pdf(x, mu)])
+        #Wasserman pg. 223
+        P = np.exp(x)/(1+np.exp(x))
+        likelihood = sum([(np.log(P[i])*y[i])+(np.log((1-P[i]))*(1-y[i])) for i in range(0, len(P))])
         print(likelihood)
      
     y_t = list(y)
     for t in range(T): 
-        '''print(p)
-        for i in range(m):
-            df[y1][i] = y[p[i]] '''
         i, j = np.random.choice(m, 2, replace = False)
-        new_l = sum([np.log(l) for l in norm.pdf([x[i], x[j]], [y_t[j], y_t[i]], sd)])
-        old_l = sum([np.log(l) for l in norm.pdf([x[i], x[j]], [y_t[i], y_t[j]], sd)])
+        P_i = np.exp(x[i])/(1+np.exp(x[i]))
+        P_j = np.exp(x[j])/(1+np.exp(x[j]))
+        new_l = (np.log(P_i)*y_t[j])+(np.log(1-P_i)*(1-y_t[j]))+(np.log(P_j)*y_t[i])+(np.log(1-P_j)*(1-y_t[i]))
+        old_l = (np.log(P_i)*y_t[i])+(np.log(1-P_i)*(1-y_t[i]))+(np.log(P_j)*y_t[j])+(np.log(1-P_j)*(1-y_t[j]))
         
         choice = min(1, np.exp(new_l - old_l))
         rand = np.random.rand()
@@ -115,11 +99,13 @@ def normal_permute(df,formula, I, T):
             
             temp = p[i]
             p[i] = p[j]
-            p[j] = temp            
-    #Returns log likelihood         
-    return([B, p, sum([np.log(l) for l in norm.pdf(x, y_t, sd)])])
+            p[j] = temp
+             
+    return([B, p, sum([(np.log(P[i])*y_t[i])+(np.log((1-P[i]))*(1-y_t[i])) for i in range(0, len(P))])])
+
+#np.prod([P[i]**y_t[i]*(1-P[i])**(1-y_t[i]) for i in range(0, len(P))])
             
-def permute_search_normal(df,formula, I, T):
+def permute_search_binom(df,formula, I, T):
     df_x = pd.DataFrame(df)
     y1 = formula.split(' ~ ')[0]
     print(y1)
@@ -131,7 +117,7 @@ def permute_search_normal(df,formula, I, T):
         if t > 0:
             new_y = build_permutation(P[t-1], list(df_x[y1]))
             df_x[y1] = new_y
-        B[t], P[t], L[t] = normal_permute(df_x,formula, 'Normal', 2000, I)
+        B[t], P[t], L[t] = logistic_permute(df_x, formula, 2000, I)
         
     return([B, P, L])
     
